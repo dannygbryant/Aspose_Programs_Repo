d91aecb35138be10614bc758ef39c4c904e510d5
danny.bryant <danny.g.bryant@gmail.com>
2017-06-05 12:59:19 -0500

    CreateWorkbook function created Main function takes argument of path and creates workbook.

--- Aspose_Cells/Aspose_Cells/Program.cs
+++ Aspose_Cells/Aspose_Cells/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aspose.Cells;
 
 namespace Aspose_Cells
 {
@@ -10,6 +11,19 @@ namespace Aspose_Cells
     {
         static void Main(string[] args)
         {
+            string path = "";
+            Console.WriteLine("Enter File Path");
+            path = Console.ReadLine();
+            Console.WriteLine(path);
+            CreateWorkbook(path);
+            Console.ReadKey();
+            //CreateWorkbook(path);
+        }
+
+        static void CreateWorkbook(string path)
+        {
+            Workbook wb = new Workbook();
+            wb.Save(path);
         }
     }
 }
