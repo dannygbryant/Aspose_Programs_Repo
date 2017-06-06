fc9d47a6fc209ee16665266b4e76dc1a24731266
danny.bryant <danny.g.bryant@gmail.com>
2017-06-06 15:17:34 -0500

    Add function to write specified text to specified Cell and save workbook

--- Aspose_Cells/Aspose_Cells/Program.cs
+++ Aspose_Cells/Aspose_Cells/Program.cs
@@ -9,21 +9,40 @@ namespace Aspose_Cells
 {
     class Program
     {
+        static string path;
         static void Main(string[] args)
         {
-            string path = "";
+            path = "";
             Console.WriteLine("Enter File Path");
             path = Console.ReadLine();
             Console.WriteLine(path);
             CreateWorkbook(path);
+
+            // Write Data to Cell
+            Console.WriteLine("Enter Cell");
+            string cell = Console.ReadLine();
+            Console.WriteLine("Enter Text");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter workbook path");
+            Workbook book = new Workbook( path );
+            WriteDataToCell(text,book,cell);
+
             Console.ReadKey();
-            //CreateWorkbook(path);
+            
         }
 
         static void CreateWorkbook(string path)
         {
             Workbook wb = new Workbook();
             wb.Save(path);
+        }
+
+        static void WriteDataToCell(string text, Workbook wb, string cell)
+        {
+            wb.Worksheets[0].Cells[cell].Value = text;
+            wb.Save(path);
+            
+            //Cell cell = new Cell();
         }
     }
 }
